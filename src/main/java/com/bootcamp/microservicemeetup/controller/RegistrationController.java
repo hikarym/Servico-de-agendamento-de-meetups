aadb2f6679a4c7f67f77ac7a8feea2e7c21d8a90
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2022-04-06 21:59:44 -0300

    feat: add controller test

--- src/main/java/com/bootcamp/microservicemeetup/controller/RegistrationController.java
+++ src/main/java/com/bootcamp/microservicemeetup/controller/RegistrationController.java
@@ -5,11 +5,18 @@ import com.bootcamp.microservicemeetup.model.entity.Registration;
 import com.bootcamp.microservicemeetup.service.RegistrationService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/registration")
@@ -68,4 +75,16 @@ public class RegistrationController {
     }
 
     // implementacao do metodo find
+    @GetMapping
+    public Page<RegistrationDTO> find(RegistrationDTO dto, Pageable pageRequest) {
+        Registration filter = modelMapper.map(dto, Registration.class);
+        Page<Registration> result = registrationService.find(filter, pageRequest);
+
+        List<RegistrationDTO> list = result.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, RegistrationDTO.class))
+                .collect(Collectors.toList());
+
+        return new PageImpl<RegistrationDTO>(list, pageRequest, result.getTotalElements());
+    }
 }
